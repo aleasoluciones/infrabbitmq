de2f539c1a3e82256271600d7b000b13bdc1c91d
regiluze <regiluze@hotmail.com>
2018-03-23 15:29:04 +0100

    requirements changes: pika rabbitmq client instead of puka and mamba and expects instead of nose and hamcrest as testing libraries

---
+++ specs/rabbitmq_spec.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from doublex import *
+from expects import *
+from doublex_expects import *
+
+
+with describe('rabbitmq client specs'):
+    with before.each:
+         pass
+    with context('when initiazling the client'):
+        with it('foo'):
+            pass
