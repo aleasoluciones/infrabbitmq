bf15bf5f0dbf591e4fd67e88c9b767eebc10b53f
Jorge Senín <jsenin@alea-soluciones.com>
2019-05-30 14:24:05 +0200

    [INTERNAL] Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(name='infrabbitmq',
       author='Bifer Team',
       description='felix rabbitmq infrastructure components',
       platforms='Linux',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      scripts=['bin/event_processor.py', 'bin/event_publisher.py', 'bin/queue_delete.py', 'bin/queue_unbind.py']
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'integration_tests']),
+      install_requires=['puka==0.0.6',
+                        'infcommon'],
+      dependency_links=['git+https://github.com/aleasoluciones/infcommon.git#egg=infcommon'],
+      scripts=['bin/event_processor.py',
+               'bin/event_publisher.py',
+               'bin/queue_delete.py',
+               'bin/queue_unbind.py']
       )
