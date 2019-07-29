f2a5a642dd46cb0f3020c0d02f3eafe428ed1ab9
Alberto Perez <aperez@alea-soluciones.com>
2019-07-29 10:24:46 +0200

    [BUGFIX] Correct bug at bin/event_publisher
    - Remove infcommon import
    - Parse to dict args.data
    - Explicit use of json_serializer

--- bin/event_publisher.py
+++ bin/event_publisher.py
@@ -2,13 +2,14 @@
 
 import argparse
 from infrabbitmq import factory
-import infcommon
 import os
+import ast
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--destination_exchange', action='store', required=True, help='')
 parser.add_argument('-e', '--event_name', action='store', default='events', help='')
-parser.add_argument('-n', '--network', action='store', default=infcommon.local_net_name(), help='')
+parser.add_argument('-n', '--network', action='store', required=True, help='Network name (ilo, c2k, ...)')
 parser.add_argument('-o', '--operations', action='store_true', default=False, help='Publish to operations broker')
 parser.add_argument("data")
 args = parser.parse_args()
@@ -19,5 +20,6 @@ if args.operations:
 else:
     broker_uri = os.environ['BROKER_URI']
 
-rabbitmq_client = factory.event_publisher(args.destination_exchange, broker_uri=broker_uri)
-rabbitmq_client.publish(args.event_name, args.network, args.data)
+rabbitmq_client = factory.event_publisher_json_serializer(args.destination_exchange, broker_uri=broker_uri)
+data_in_dictionary_format = ast.literal_eval(args.data)
+rabbitmq_client.publish(args.event_name, args.network, data_in_dictionary_format)
